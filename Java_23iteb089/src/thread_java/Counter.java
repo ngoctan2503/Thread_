1e77f08e4d7b2a677dc101aa88da16d11316f908
ngoctan2503 <161423694+ngoctan2503@users.noreply.github.com>
2024-03-05 17:07:04 +0700

    upload_file

---
+++ Java_23iteb089/src/thread_java/Counter.java
@@ -0,0 +1,15 @@
+package thread_java;
+
+public class Counter {
+	private int count = 0;
+	
+	public synchronized void increment() throws InterruptedException{
+		count++;
+		System.out.print(" " + count + " ");
+		Thread.sleep(1000);
+	}
+	
+	public int getCount() {
+		return count;
+	}
+}
